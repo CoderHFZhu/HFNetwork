9f998c243f76e0a384dc36abc2011effa8e22220
CoderHF <54666674@qq.com>
2018-09-12 23:57:48 +0800

    Initial Commit

---
+++ HFNetworkDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFNetworkDemo
+//
+//  Created by CoderHF on 2018/9/12.
+//  Copyright © 2018年 CoderHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
